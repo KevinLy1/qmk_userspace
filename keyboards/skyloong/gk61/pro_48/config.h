dd0e71e5c824d32cd9cac57d423419e217ef70f3
Kevin Ly <contact@kevinly.fr>
2025-07-06 21:40:07 +0200

    Add custom firmware

---
+++ keyboards/skyloong/gk61/pro_48/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 linlin012 (@linlin012)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define IS31FL3743A_I2C_ADDRESS_1 IS31FL3743A_I2C_ADDRESS_GND_GND
+#define IS31FL3743A_SDB_PIN A4
+
+#define CAPS_LOCK_INDEX 28
+
+#define TAPPING_TERM 200
+#define CHORDAL_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
